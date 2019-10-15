be641cd45801950c8cdc4fe6ae8e442d3bd09bbf
菅野裕介 <kanno.yusuke@MacBook-Pro.local>
2019-10-15 14:29:33 +0900

    [Finish]5-6

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9088cd0f5e30399edab30eb5b4eba631d328eaab12ee4e71e6302597e84be2a63d4ac5007fee20583ba735d2546e5cd65d99d4e5cebaffc641a53251f339be8d'
